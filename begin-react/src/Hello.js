74a39acda5282904259d50b5800659dd7993d221
Noh-jisu <nojisu123@naver.com>
2022-10-15 21:03:29 +0900

    조건부 렌더링 isSpecial

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 // props 대신 {color, name} 사용가능
-function Hello({color, name} ) {
-    return <div style={{
-        // props.color 대신 color사용가능
-        color
-    }}>안녕하세요 {name}</div>
+function Hello({color, name, isSpecial} ) {
+    return (
+        <div style={{
+            // props.color 대신 color사용가능
+            color
+        }}>
+            {/*{isSpecial ? <b>*</b> : null}*/}
+            {isSpecial && <b>*</b>}
+            안녕하세요 {name}
+        </div>
+    );
 }
 // props를 설정하지 않았을때 기본값 설정방법
 Hello.defaultProps = {
--- begin-react/src/Wrapper.js
+++ begin-react/src/Wrapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// children사용 시 App.js에서 <Wrapper></Wrapper>사이에 있는 값 가져올 수 있음.
+// children사용 시 App.js에서 <Wrapper></Wrapper>사이에 있는 값 가져올 수 있음. 
 function Wrapper( {children} ) {
     const style = {
         border : "2px solid black",
