ab6e949427c16f0e287df9d11bfe880deaaba70d
Noh-jisu <nojisu123@naver.com>
2022-10-15 20:24:25 +0900

    JSX규칙 자바스크립트값 사용방법 외부스타일시트 사용방법 주석 사용방법

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Hello from "./Hello";
+import './App.css'; 
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize : 24,
+    padding : '1rem'
+  }
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      {/* 주석을 사용할땐 {}안에 /+* 로 작성 */}
+      <Hello 
+        // 태그가 열리는부분에서 싱글라인태그도 사용 가능
+      />
+      {/* JSX내부에서 JavaScript값을 사용하는 방법 */}
+      <div style={style}>{name}</div>
+      {/* 외부 styleSheet사용 예시 */}
+      <div className="gray-box"></div>
+    </>
   );
 }
 
