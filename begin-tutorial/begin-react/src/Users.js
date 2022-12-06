208d77c7c6d6a9cd6e90804bf3ed22f27f18ab6b
DESKTOP-OCFBCRG\jisuNoh <nojisu123@naver.com>
2022-12-06 23:00:56 +0900

    context에서 비동기작업 상태관리하기 (user.js에는 적용 예정)

--- begin-tutorial/begin-react/src/Users.js
+++ begin-tutorial/begin-react/src/Users.js
@@ -1,21 +1,20 @@
 import React, {useState} from 'react';
-import axios from 'axios';
-import useAsync from './useAsync';
 import User from './User';
+import { getUsers, useUsersDispatch, useUsersState } from './UsersContext';
 
-async function getUsers() {
-    const response = axios.get('https://jsonplaceholder.typicode.com/users/');
-    return (await response).data;
-}
-
-function Users () {
-    const [state, refetch] = useAsync(getUsers, [], true);
-    const {loading, data: users, error} = state;
+function Users() {
     const [userId, setUserId] = useState(null);
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
 
+    const { loading, data: users, error } = state.users;
+
+    const fetchData = () => {
+        getUsers(dispatch);
+    }
     if(loading) return <div>로딩중...</div>
     if(error) return <div>에러가 발생했습니다.</div>
-    if(!users) return <button onClick={refetch}>불러오기</button>;
+    if(!users) return <button onClick={fetchData}>불러오기</button>;
 
     return (
         <>
@@ -27,7 +26,7 @@ function Users () {
             </li>
             ))}
         </ul>
-        <button onClick={refetch}>다시 불러오기</button>
+        <button onClick={fetchData}>다시 불러오기</button>
         <hr />
         {userId && <User id={userId} />}
         </>
